1ad52d7b14addc521eec02a1fe983f91a397056c
toneiojimz <ajimeneza77@gmail.com>
2021-02-10 14:56:37 -0800

    created new directoriies and moved page components

--- about.tsx
+++
@@ -1,7 +0,0 @@
-import React from  "react";
-
-const About = () => {
-    return <h1>About me and my food adventures</h1>
-};
-
-export default About;
--- travel.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Travel = () => {
-    return <h1>Where should we go next?</h1>
-};
-
-
-export default Travel;
